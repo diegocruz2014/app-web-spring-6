35fdac51573fe3642acf64738640614f2403939b
Diego Cruz <diegocruzo@gmail.com>
2024-09-09 15:44:22 -0500

    Primer commit desde rama jpa-entities

---
+++ src/main/java/medellin/biblioteca/appwebspring6/domain/Autor.java
@@ -0,0 +1,43 @@
+package medellin.biblioteca.appwebspring6.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id; // Este campo será usado para la persistencia en la base de datos
+    private String nombres;
+    private String apellidos;
+
+    // Getter and Setter
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
---
+++ src/main/java/medellin/biblioteca/appwebspring6/domain/Libro.java
@@ -0,0 +1,42 @@
+package medellin.biblioteca.appwebspring6.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String titulo;
+    private String isbn;
+
+    // Getter and Setter
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
